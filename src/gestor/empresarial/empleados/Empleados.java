c51a33441324d1b8ef6f0909fb931b8c9669c821
BeMoBu <s.storm2528@gmail.com>
2024-04-08 22:25:17 -0600

    Version 2.1 funcional

--- src/gestor/empresarial/empleados/Empleados.java
+++ src/gestor/empresarial/empleados/Empleados.java
@@ -3,16 +3,68 @@ import gestor.errores.GestionErrores;
 import gestor.empresarial.contrato.*;
 import gestor.empresarial.datos.DatosPersonales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Empleados implements iEmpleados{
     private int i;
     public GestionErrores error;
+    private static Empleados instancia = null; // Creamos una istancia de la propia clase para aplicar el patrón de diseño Singleton
 
-    public Empleados(){
+    //Creamos listas para guardar los DATOS PERSONALES
+    private List<Integer> id; //Lista de ids
+    private List<String> nombre; //Lista de nombres
+    private List<String> whatsapp; //Lista de numeros de whatsapp
+    private List<String> correo; //Lista de correos electronicos
 
+    public Empleados() { //Constructor
+        // Aqui inicializamos las listas
+        id = new ArrayList<>();
+        nombre = new ArrayList<>();
+        whatsapp = new ArrayList<>();
+        correo = new ArrayList<>();
     }
-    public void addDatosPersonales(String nombre, String correo, String whatsapp){
 
+    public static Empleados getInstancia() { //Instancia unica para mantener los datos guardados toda la ejecucion del programa
+        if (instancia == null) {
+            instancia = new Empleados();
+        }
+        return instancia;
     }
+
+    //METODOS PARA AGREGAR LOS DATOS
+    public void addDatosPersonales(int id, String nombre, String whatsapp, String email) { //Metodo para añadir los datos personales
+        //Con esta clase agregamos los datos a las listas
+        this.id.add(id);
+        this.nombre.add(nombre);
+        this.whatsapp.add(whatsapp);
+        this.correo.add(email);
+    }
+
+    public void imprimirDatos() {
+        System.out.println("Datos guardados en DatosPersonales:");
+        for (int i = 0; i < id.size(); i++) {
+            System.out.println("ID: " + id.get(i) + ", Nombre: " + nombre.get(i) + ", WhatsApp: " + whatsapp.get(i) + ", Email: " + correo.get(i));
+        }
+    }
+
+    //Metodos get para accesar a los valores de las listas
+    public List<Integer> getId() {
+        return id;
+    }
+
+    public List<String> getNombre() {
+        return nombre;
+    }
+
+    public List<String> getCorreo() {
+        return correo;
+    }
+
+    public List<String> getWhatsapp() {
+        return whatsapp;
+    }
+
     public void addContrato(int id, int noContrato, String horario,Cargos tipoCargo){
 
     }
